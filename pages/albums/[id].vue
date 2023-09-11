91e2af8cda76e25770abce9fcf398fe8b2dc3fd8
Dane <875126243@qq.com>
2023-09-11 11:28:02 +0800

    fix update albums

--- pages/albums/[id].vue
+++ pages/albums/[id].vue
@@ -1,25 +1,86 @@
 <template>
        <div class="w-[1024px] mt-20 bg-slate-50 dark:bg-slate-700 rounded-lg md:rounded-xl mx-3 lg:mx-0 shadow-lg md:shadow-xl p-3 xs:p-6 md:p-9">
-    
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div v-for="image in images" :key="image.id" class="bg-gray-200 p-4 rounded-lg">
-            <img :src="image.url" :alt="image.alt" class="w-full h-auto">
+
+        <!-- ================================================= -->
+        <div v-if="loading">
+            <el-skeleton :loading="loading" animated>
+                    <template #template>
+                        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                            <el-skeleton-item variant="image" style="height: 240px" v-for="i in  7"  class="rounded-lg shadow-lg cursor-pointer"/>
+                        </div>
+                    </template>
+                </el-skeleton>
         </div>
-    </div>
+        <div v-else>
+            <div v-if="lists.length > 0">
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                        <el-image
+                            v-for="(item, index) in lists" :key="index" :alt="item.title"
+                            class="rounded-lg shadow-lg cursor-pointer"
+                            :src="item.image" 
+                            lazy
+                            :zoom-rate="1.2"
+                            :preview-src-list="[item.image]"
+                            :initial-index="0"
+                            fit="cover"
+                            />
+                    </div>
+                    <div class="flex justify-center items-center my-3"  v-if="totalPages > 1">
+                            <el-pagination 
+                                small
+                                background
+                                v-model:currentPage="currentPage" 
+                                :page-size ="pageSize"
+                                layout="prev, pager, next"
+                                :total="countPage"
+                                :page-count="totalPages"
+                                @current-change="handleCurrentChange"
+                                class="mt-4"
+                            />
+                    </div>
+                </div>
+                <el-empty v-else description="还没有数据" />
+        </div>
+        <!-- ================================================= -->
 
     </div>
 </template>
 
 <script setup>
-const images =  [
-        { id: 1, url: 'https://via.placeholder.com/100x100', alt: 'Image 1' },
-        { id: 2, url: 'https://via.placeholder.com/100x100', alt: 'Image 2' },
-        { id: 3, url: 'https://via.placeholder.com/100x100', alt: 'Image 3' },
-        { id: 1, url: 'https://via.placeholder.com/100x100', alt: 'Image 1' },
-        { id: 2, url: 'https://via.placeholder.com/100x100', alt: 'Image 2' },
-        { id: 3, url: 'https://via.placeholder.com/100x100', alt: 'Image 3' },
-        { id: 1, url: 'https://via.placeholder.com/100x100', alt: 'Image 1' },
-        { id: 2, url: 'https://via.placeholder.com/100x100', alt: 'Image 2' },
-        { id: 3, url: 'https://via.placeholder.com/100x100', alt: 'Image 3' }
-      ]
+const { albumsApi } = useApi()
+const lists = ref([])
+
+const currentPage = ref('')
+const countPage = ref('')
+const totalPages = ref(0)
+const pageSize = 12
+const loading = ref(false)
+const router = useRouter()
+const route = useRoute()
+
+
+const loadData = async (page_no) => {
+    loading.value = true
+    const params = {
+        "id": Number(route.params.id),
+        "page_no": Number(page_no) ? Number(page_no) : 1,
+        "page_size": pageSize,
+    }
+    const { data } =  await albumsApi.detail(params)
+    totalPages.value = Math.ceil(data.count / pageSize)
+    lists.value = data.lists
+    countPage.value = data.count
+    currentPage.value = data.page_no
+    loading.value = false
+}
+
+loadData(1)
+
+const handleClickDetail = (id)=> {
+    router.push({ name: 'albums-id', params: { id } });
+}
+
+const handleCurrentChange = (id)=> {
+  loadData(id)
+}
 </script>
--- pages/albums/index.vue
+++ pages/albums/index.vue
@@ -1,37 +1,48 @@
 <template>
     <div class="w-[1024px] md:min-h-[750px] mt-20 bg-slate-50 dark:bg-slate-700 rounded-lg md:rounded-xl mx-3 lg:mx-0 shadow-lg md:shadow-xl p-3 xs:p-6 md:p-9">
-          <div  v-if="lists.length > 0">
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                      <div v-for="(album, index) in lists" :key="index" class="rounded-lg overflow-hidden shadow-md cursor-pointer" @click="handleClickDetail(album.id)">
-                          <div class="aspect-w-16 aspect-h-9 bg-slate-200">
-                              <div class="flex items-center justify-center h-full">
-                              <div class="w-48 h-32">
-                                  <img :src="album.image" :alt="album.title" class="object-contain w-full h-full" />
-                              </div>
-                              </div>
-                          </div>
-                          <div class="p-4 bg-white">
-                              <h2 class="text-xl font-semibold">{{ album.title }}</h2>
-                              <p class="text-gray-500">{{ album.description }}</p>
-                          </div>
-                      </div>
-                  </div>
-                  
-                  <div class="flex justify-center items-center my-3" >
-                          <el-pagination 
-                              small
-                              background
-                              v-model:currentPage="currentPage" 
-                              :page-size ="pageSize"
-                              layout="prev, pager, next"
-                              :total="countPage"
-                              :page-count="totalPages"
-                              @current-change="handleCurrentChange"
-                              class="mt-4"
-                          />
-                  </div>
+            <div v-if="loading">
+                <el-skeleton  :loading="loading" animated>
+                        <template #template>
+                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                                <el-skeleton-item variant="image" style="height: 240px" v-for="i in  7"  class="rounded-lg shadow-lg cursor-pointer"/>
+                            </div>
+                        </template>
+                    </el-skeleton>
             </div>
-            <el-empty v-else description="no data" />
+            <div v-else>
+                <div  v-if="lists.length > 0">
+                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                                <div v-for="(album, index) in lists" :key="index" class="rounded-lg overflow-hidden shadow-md cursor-pointer" @click="handleClickDetail(album.id)">
+                                    <div class="aspect-w-16 aspect-h-9 bg-slate-200">
+                                        <div class="flex items-center justify-center h-full">
+                                        <div class="w-48 h-32">
+                                            <img :src="album.image" :alt="album.title" class="object-contain w-full h-full" />
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div class="p-4 bg-white">
+                                        <h2 class="text-xl font-semibold">{{ album.title }}</h2>
+                                        <p class="text-gray-500">{{ album.description }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="flex justify-center items-center my-3" >
+                                    <el-pagination 
+                                        small
+                                        background
+                                        v-model:currentPage="currentPage" 
+                                        :page-size ="pageSize"
+                                        layout="prev, pager, next"
+                                        :total="countPage"
+                                        :page-count="totalPages"
+                                        @current-change="handleCurrentChange"
+                                        class="mt-4"
+                                    />
+                            </div>
+                        </div>
+                        <el-empty v-else description="no data" />
+            </div>
 
     </div>
 </template>     
@@ -40,22 +51,26 @@
 const { albumsApi } = useApi()
 const lists = ref([])
 
+const loading = ref(false)
 const currentPage = ref('')
 const countPage = ref('')
-const totalPages = ref('')
+const totalPages = ref(0)
 const pageSize = 9
 
 const router = useRouter()
 
 const loadData = async (page_no) => {
+    loading.value = true
     const params = {
         "page_no": Number(page_no) ? Number(page_no) : 1,
         "page_size": pageSize,
     }
     const { data } =  await albumsApi.lists(params)
+    totalPages.value = Math.ceil(data.count / pageSize)
     lists.value = data.lists
     countPage.value = data.count
     currentPage.value = data.page_no
+    loading.value = false
 }
 
 loadData(1)
