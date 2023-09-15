de78c699e42555412dce7977bbbce08c00e5c992
Dane <875126243@qq.com>
2023-09-15 17:18:36 +0800

    fix update chat

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="w-full">
-
+           <canvas ref="canvas"></canvas>
    </div>    
 </template>
 
@@ -9,109 +9,180 @@ import * as THREE from 'three';
 import * as YUKA from 'yuka';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-const scene = new THREE.Scene();
-
-renderer.setClearColor(0xA3A3A3);
-
-const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-
-camera.position.set(0, 10, 15);
-camera.lookAt(scene.position);
-
-const ambientLight = new THREE.AmbientLight(0x333333);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-directionalLight.position.set(0, 10, 10);
-scene.add(directionalLight);
-
-const vehicle = new YUKA.Vehicle();
-
-function sync(entity, renderComponent) {
-    renderComponent.matrix.copy(entity.worldMatrix);
-}
-
-const path = new YUKA.Path();
-path.add( new YUKA.Vector3(-6, 0, 4));
-path.add( new YUKA.Vector3(-12, 0, 0));
-path.add( new YUKA.Vector3(-6, 0, -12));
-path.add( new YUKA.Vector3(0, 0, 0));
-path.add( new YUKA.Vector3(8, 0, -8));
-path.add( new YUKA.Vector3(10, 0, 0));
-path.add( new YUKA.Vector3(4, 0, 4));
-path.add( new YUKA.Vector3(0, 0, 6));
-
-path.loop = true;
-
-vehicle.position.copy(path.current());
-
-vehicle.maxSpeed = 3;
-
-const followPathBehavior = new YUKA.FollowPathBehavior(path, 3);
-vehicle.steering.add(followPathBehavior);
-
-const onPathBehavior = new YUKA.OnPathBehavior(path);
-//onPathBehavior.radius = 2;
-vehicle.steering.add(onPathBehavior);
-
-const entityManager = new YUKA.EntityManager();
-entityManager.add(vehicle);
-
-const loader = new GLTFLoader();
-loader.load('./assets/SUV.glb', function(glb) {
-    const model = glb.scene;
-    //model.scale.set(0.5, 0.5, 0.5);
-    scene.add(model);
-    model.matrixAutoUpdate = false;
-    vehicle.scale = new YUKA.Vector3(0.5, 0.5, 0.5);
-    vehicle.setRenderComponent(model, sync);
+definePageMeta({
+    layout: 'no-header-footer',
+    index: 1,
 });
 
-// const vehicleGeometry = new THREE.ConeBufferGeometry(0.1, 0.5, 8);
-// vehicleGeometry.rotateX(Math.PI * 0.5);
-// const vehicleMaterial = new THREE.MeshNormalMaterial();
-// const vehicleMesh = new THREE.Mesh(vehicleGeometry, vehicleMaterial);
-// vehicleMesh.matrixAutoUpdate = false;
-// scene.add(vehicleMesh);
+const canvas = ref(null)
 
-const position = [];
-for(let i = 0; i < path._waypoints.length; i++) {
-    const waypoint = path._waypoints[i];
-    position.push(waypoint.x, waypoint.y, waypoint.z);
-}
 
-const lineGeometry = new THREE.BufferGeometry();
-lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
+onMounted(() => {
+    let scene, camera, renderer
+    // 创建场景
+    scene = new THREE.Scene();
+    // 设置背景图片
+    const loader = new THREE.TextureLoader();
+    loader.load('./images/bg.jpg', (texture) => {
+        scene.background = texture;
+    });
 
-const lineMaterial = new THREE.LineBasicMaterial({color: 0xFFFFFF});
-const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
-scene.add(lines);
+    // 创建相机
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
-const time = new YUKA.Time();
-
-function animate() {
-    const delta = time.update().getDelta();
-    entityManager.update(delta);
-    renderer.render(scene, camera);
-}
-
-renderer.setAnimationLoop(animate);
-
-window.addEventListener('resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    // 创建渲染器
+    renderer = new THREE.WebGLRenderer({ canvas: canvas.value });
     renderer.setSize(window.innerWidth, window.innerHeight);
-});
+
+
+    // scene.add(cube);
+    const planeGeometry = new THREE.PlaneGeometry( 2, 2 );
+    // 使用TextureLoader加载图片纹理
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load('./images/bg.jpg'); 
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const plane = new THREE.Mesh( planeGeometry, material );
+    scene.add( plane );
+
+    
+    // 创建一个变量来保存原始平面的缩放
+    const originalScale = plane.scale.clone();
+
+    // 创建一个射线投射器
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    // 添加鼠标移动事件监听器
+    document.addEventListener('mousemove', (event) => {
+    // 将鼠标坐标转换为Three.js坐标系中的坐标
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    // 更新射线投射器的位置
+    raycaster.setFromCamera(mouse, camera);
+
+    // 检测鼠标与平面的交点
+    const intersects = raycaster.intersectObject(plane);
+
+    if (intersects.length > 0) {
+        // 鼠标移入时放大平面
+        plane.scale.set(originalScale.x * 1.2, originalScale.y * 1.2, 1);
+    } else {
+        // 鼠标移出时恢复原始缩放
+        plane.scale.copy(originalScale);
+    }
+    });
+
+    // 使平面可接收鼠标事件
+    plane.userData.interactive = true;
+
+    // 添加动画循环
+    const animate = () => {
+        requestAnimationFrame(animate);
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.render(scene, camera);
+    };
+    animate();
+})
+
+// const renderer = new THREE.WebGLRenderer({antialias: true});
+
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+// document.body.appendChild(renderer.domElement);
+
+// const scene = new THREE.Scene();
+
+// renderer.setClearColor(0xA3A3A3);
+
+// const camera = new THREE.PerspectiveCamera(
+//     45,
+//     window.innerWidth / window.innerHeight,
+//     0.1,
+//     1000
+// );
+
+// camera.position.set(0, 10, 15);
+// camera.lookAt(scene.position);
+
+// const ambientLight = new THREE.AmbientLight(0x333333);
+// scene.add(ambientLight);
+
+// const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+// directionalLight.position.set(0, 10, 10);
+// scene.add(directionalLight);
+
+// const vehicle = new YUKA.Vehicle();
+
+// function sync(entity, renderComponent) {
+//     renderComponent.matrix.copy(entity.worldMatrix);
+// }
+
+// const path = new YUKA.Path();
+// path.add( new YUKA.Vector3(-6, 0, 4));
+// path.add( new YUKA.Vector3(-12, 0, 0));
+// path.add( new YUKA.Vector3(-6, 0, -12));
+// path.add( new YUKA.Vector3(0, 0, 0));
+// path.add( new YUKA.Vector3(8, 0, -8));
+// path.add( new YUKA.Vector3(10, 0, 0));
+// path.add( new YUKA.Vector3(4, 0, 4));
+// path.add( new YUKA.Vector3(0, 0, 6));
+
+// path.loop = true;
+
+// vehicle.position.copy(path.current());
+
+// vehicle.maxSpeed = 3;
+
+// const followPathBehavior = new YUKA.FollowPathBehavior(path, 3);
+// vehicle.steering.add(followPathBehavior);
+
+// const onPathBehavior = new YUKA.OnPathBehavior(path);
+// //onPathBehavior.radius = 2;
+// vehicle.steering.add(onPathBehavior);
+
+// const entityManager = new YUKA.EntityManager();
+// entityManager.add(vehicle);
+
+// const loader = new GLTFLoader();
+// loader.load('./assets/SUV.glb', function(glb) {
+//     const model = glb.scene;
+//     //model.scale.set(0.5, 0.5, 0.5);
+//     scene.add(model);
+//     model.matrixAutoUpdate = false;
+//     vehicle.scale = new YUKA.Vector3(0.5, 0.5, 0.5);
+//     vehicle.setRenderComponent(model, sync);
+// });
+
+// const position = [];
+// for(let i = 0; i < path._waypoints.length; i++) {
+//     const waypoint = path._waypoints[i];
+//     position.push(waypoint.x, waypoint.y, waypoint.z);
+// }
+
+// const lineGeometry = new THREE.BufferGeometry();
+// lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
+
+// const lineMaterial = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+// const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
+// scene.add(lines);
+
+// const time = new YUKA.Time();
+
+// function animate() {
+//     const delta = time.update().getDelta();
+//     entityManager.update(delta);
+//     renderer.render(scene, camera);
+// }
+
+// renderer.setAnimationLoop(animate);
+
+// window.addEventListener('resize', function() {
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+// });
 
 </script>
--- socketIo/index.js
+++ socketIo/index.js
@@ -1,18 +1,127 @@
-const uWS = require("uWebSockets.js");
-const { Server } = require("socket.io");
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const createAdapter = require("@socket.io/redis-adapter").createAdapter;
+const redis = require("redis");
+const jwt = require('jsonwebtoken');
 
-const app = uWS.App();
-const io = new Server();
+require("dotenv").config();
+const { createClient } = redis;
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
-io.attachApp(app);
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
+// const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjE2LCJleHAiOjE2OTQ0MTU0NTN9.97xCqwV9KvazYJx15KVVwEwbHrw2yEVTec4o-ROfraI'; // 替换为要验证的PHP生成的JWT令牌
+// const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjE2LCJleHAiOjE2OTQ2NjkwNjh9.3cfk6NCI65xX5n2UatLkvWKWJNdzz_d76W5SVEnmF-Q'; // 替换为要验证的PHP生成的JWT令牌
+const publicKey = 'secret-key'; // 替换为与PHP生成JWT时使用的公钥
+
+// try {
+
+//   // ----------------------------------------------------------------------------
+//     // var tokensign = jwt.sign({ uid: 2 }, publicKey, { algorithm: 'HS256' });
+//   // console.log('tokensign:', tokensign);
+//   // ----------------------------------------------------------------------------
+//   // jwt.verify(token, publicKey, { algorithms: ['HS256'] }, function (err, payload) {
+//   //   // if token alg != RS256,  err == invalid signature
+//   //   console.log("err", err)
+//   //   console.log("payload", payload)
+//   // });
+//   // ----------------------------------------------------------------------------
+//   const decoded = jwt.verify(token, publicKey, { algorithms: ['HS256'] });
+//   console.log('JWT验证成功:', decoded);
+//   // ----------------------------------------------------------------------------
+// } catch (error) {
+//   console.log('JWT验证失败:', error);
+// }
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "Chat";
+
+(async () => {
+  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+  await pubClient.connect();
+  subClient = pubClient.duplicate();
+  io.adapter(createAdapter(pubClient, subClient));
+})();
+
+// Run when client connects
 io.on("connection", (socket) => {
-  // ...
-  console.log("=====");
+  // console.log(io.of("/").adapter);
+  const token = socket.handshake.query.token;
+  // console.log('token:', token);
+  // if(token){ 
+  //     try {
+  //         // ----------------------------------------------------------------------------
+  //         const decoded = jwt.verify(token, publicKey, { algorithms: ['HS256'] });
+  //         console.log('JWT验证成功:', decoded);
+  //         // ----------------------------------------------------------------------------
+  //     } catch (error) {
+  //         console.log('JWT验证失败:', error);
+  //     }
+  // }
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to EZPalay!"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+        io.to(user.room).emit(
+          "message",
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
+
+        // Send users and room info
+        io.to(user.room).emit("roomUsers", {
+          room: user.room,
+          users: getRoomUsers(user.room),
+        });
+    }
+  });
 });
 
-app.listen(3010, (token) => {
-  if (!token) {
-    console.warn("端口已被使用");
-  }
-});
+const PORT = 3030;
+
+server.listen(
+    PORT, 
+    () => 
+    console.log(`Server running on port ${PORT}`)
+);
