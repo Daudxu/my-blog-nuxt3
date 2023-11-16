7bae3aa31c97132228ee94f874ede9f90550591d
dan <875126243@qq.com>
2023-11-16 14:27:44 +0800

    fix update code

--- plugins/baidu.js
+++ plugins/baidu.js
@@ -1,5 +1,4 @@
 export default defineNuxtPlugin(() => {
-
     // window._hmt = window._hmt || []
     const hm = document.createElement('script')
     hm.src = "https://hm.baidu.com/hm.js?1eab1937a49bb008b1cfc99c774200aa";
